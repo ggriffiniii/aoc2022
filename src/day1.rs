6108f1dd502fc669facfe5e8a03d23c17bfa3f8b
Glenn Griffin <ggriffiniii@gmail.com>
2022-12-02 19:52:29 -0800

    day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,29 +1,46 @@
 use aoc_runner_derive::aoc;
 
+struct ElfIter<'a>(std::str::Bytes<'a>);
+impl<'a> ElfIter<'a> {
+    fn new(input: &'a str) -> Self {
+        ElfIter(input.bytes())
+    }
+}
+
+impl<'a> Iterator for ElfIter<'a> {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut sum = 0;
+        loop {
+            let mut calories = 0;
+            loop {
+                match self.0.next() {
+                    None => return None,
+                    Some(b) if b == b'\n' => {
+                        if calories == 0 {
+                            return Some(sum);
+                        }
+                        sum += calories;
+                        calories = 0;
+                    }
+                    Some(b) => {
+                        calories *= 10;
+                        calories += (b - b'0') as usize;
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> usize {
-    input
-        .split("\n\n")
-        .map(|elf_record| {
-            elf_record
-                .split('\n')
-                .map(|calories| -> usize { calories.parse().unwrap() })
-                .sum()
-        })
-        .max()
-        .unwrap()
+    ElfIter::new(input).max().unwrap()
 }
 
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> usize {
-    input
-        .split("\n\n")
-        .map(|elf_record| {
-            elf_record
-                .split('\n')
-                .map(|calories| -> usize { calories.parse().unwrap() })
-                .sum()
-        })
+    ElfIter::new(input)
         .fold([0, 0, 0], |top3, x| match top3 {
             [a, b, c] if x < c => [a, b, c],
             [a, b, _] if x < b => [a, b, x],
