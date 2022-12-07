b1dde92a494accee2ca2c24fef39a3366e915fed
Glenn Griffin <ggriffiniii@gmail.com>
2022-12-07 08:22:06 -0800

    day7 refcell implementation

--- src/day7.rs
+++ src/day7.rs
@@ -1,108 +1,102 @@
-use std::{convert::Infallible, str::FromStr};
+use std::{cell::RefCell, rc::Rc};
 
 use aoc_runner_derive::aoc;
 
 #[derive(Debug, Default)]
 struct Dir {
-    idx: usize,
-    parent_dir: Option<usize>,
-    child_dirs: Vec<(String, usize)>,
+    parent_dir: Option<Rc<RefCell<Dir>>>,
+    child_dirs: Vec<(String, Rc<RefCell<Dir>>)>,
     files: Vec<(String, usize)>,
 }
 
-struct Filesystem(Vec<Dir>);
-impl FromStr for Filesystem {
-    type Err = Infallible;
-
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let mut dirs = vec![Dir {
-            idx: 0,
-            parent_dir: None,
-            child_dirs: Vec::new(),
-            files: Vec::new(),
-        }];
-        let mut current_dir = 0;
-        for line in input.lines() {
-            match line.split_once(' ').unwrap() {
-                ("$", cmd) => {
-                    if cmd == "ls" {
-                        // do nothing
-                    } else if let Some(dirname) = cmd.strip_prefix("cd ") {
-                        current_dir = match dirname {
-                            "/" => 0,
-                            ".." => dirs[current_dir].parent_dir.unwrap(),
-                            _ => {
-                                dirs[current_dir]
-                                    .child_dirs
-                                    .iter()
-                                    .find(|(name, _idx)| name == dirname)
-                                    .unwrap()
-                                    .1
-                            }
-                        };
-                    }
-                }
-                ("dir", name) => {
-                    let idx = dirs.len();
-                    dirs.push(Dir {
-                        idx,
-                        parent_dir: Some(current_dir),
-                        child_dirs: Vec::new(),
-                        files: Vec::new(),
-                    });
-                    dirs[current_dir].child_dirs.push((name.to_owned(), idx));
-                }
-                (size, filename) => {
-                    dirs[current_dir]
-                        .files
-                        .push((filename.to_owned(), size.parse().unwrap()));
-                }
-            }
-        }
-        Ok(Filesystem(dirs))
+fn dirs(top_dir: Rc<RefCell<Dir>>) -> Vec<Rc<RefCell<Dir>>> {
+    // this would be better as an iterator, but I don't have the energy
+    let mut all_dirs = vec![Rc::clone(&top_dir)];
+    for (_name, dir) in top_dir.borrow().child_dirs.iter() {
+        all_dirs.extend(dirs(Rc::clone(dir)));
     }
+    all_dirs
 }
-
-impl Filesystem {
-    fn dirs(&self) -> std::slice::Iter<'_, Dir> {
-        self.0.iter()
-    }
-
+impl Dir {
     fn total_size(&self) -> usize {
-        self.size_of_dir(&self.0[0])
-    }
-
-    fn size_of_dir(&self, dir: &Dir) -> usize {
-        self.0[dir.idx]
-            .files
-            .iter()
-            .map(|(_name, size)| size)
-            .sum::<usize>()
-            + self.0[dir.idx]
+        self.files.iter().map(|(_name, size)| size).sum::<usize>()
+            + self
                 .child_dirs
                 .iter()
-                .map(|(_name, idx)| self.size_of_dir(&self.0[*idx]))
+                .map(|(_name, dir)| dir.borrow().total_size())
                 .sum::<usize>()
     }
 }
 
+fn parse_filesystem(input: &str) -> Rc<RefCell<Dir>> {
+    let root = Rc::new(RefCell::new(Dir {
+        parent_dir: None,
+        child_dirs: Vec::new(),
+        files: Vec::new(),
+    }));
+    let mut current_dir = Rc::clone(&root);
+    for line in input.lines() {
+        match line.split_once(' ').unwrap() {
+            ("$", cmd) => {
+                if cmd == "ls" {
+                    // do nothing
+                } else if let Some(dirname) = cmd.strip_prefix("cd ") {
+                    current_dir = match dirname {
+                        "/" => Rc::clone(&root),
+                        ".." => Rc::clone(current_dir.borrow().parent_dir.as_ref().unwrap()),
+                        _ => Rc::clone(
+                            &current_dir
+                                .borrow()
+                                .child_dirs
+                                .iter()
+                                .find(|(name, _idx)| name == dirname)
+                                .unwrap()
+                                .1,
+                        ),
+                    };
+                }
+            }
+            ("dir", name) => {
+                let child_dir = Rc::new(RefCell::new(Dir {
+                    parent_dir: Some(Rc::clone(&current_dir)),
+                    child_dirs: Vec::new(),
+                    files: Vec::new(),
+                }));
+                current_dir
+                    .borrow_mut()
+                    .child_dirs
+                    .push((name.to_owned(), child_dir));
+            }
+            (size, filename) => {
+                current_dir
+                    .borrow_mut()
+                    .files
+                    .push((filename.to_owned(), size.parse().unwrap()));
+            }
+        }
+    }
+    root
+}
+
 #[aoc(day7, part1)]
 pub fn part1(input: &str) -> usize {
-    let fs: Filesystem = input.parse().unwrap();
-    fs.dirs()
-        .map(|dir| fs.size_of_dir(dir))
+    let root_dir = parse_filesystem(input);
+    dirs(root_dir)
+        .iter()
+        .map(|dir| dir.borrow().total_size())
         .filter(|&size| size <= 100000)
         .sum()
 }
 
 #[aoc(day7, part2)]
 pub fn part2(input: &str) -> usize {
-    let fs: Filesystem = input.parse().unwrap();
-    let space_used = fs.total_size();
+    let root_dir = parse_filesystem(input);
+    let space_used = root_dir.borrow().total_size();
     let unused_space = 70000000 - space_used;
     let min_to_delete = 30000000 - unused_space;
-    fs.dirs()
-        .map(|dir| fs.size_of_dir(dir))
+    dirs(root_dir)
+        .iter()
+        .map(|dir| dir.borrow().total_size())
         .filter(|&size| size >= min_to_delete)
         .min()
         .unwrap()
