b98044004857c9bbd9009d9522cfdffb97c269f9
Glenn Griffin <ggriffiniii@gmail.com>
2022-12-12 22:13:27 -0800

    day13

---
+++ src/day13.rs
@@ -0,0 +1,100 @@
+use std::cmp::Ordering;
+
+use aoc_runner_derive::aoc;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum ListOrInt {
+    List(Vec<ListOrInt>),
+    Int(usize),
+}
+impl Ord for ListOrInt {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (ListOrInt::Int(left), ListOrInt::Int(right)) => left.cmp(right),
+            (ListOrInt::List(left), ListOrInt::List(right)) => left.cmp(right),
+            (ListOrInt::Int(left), ListOrInt::List(right)) => {
+                let left = [ListOrInt::Int(*left)];
+                left.as_slice().cmp(right.as_slice())
+            }
+            (ListOrInt::List(left), ListOrInt::Int(right)) => {
+                let right = [ListOrInt::Int(*right)];
+                left.as_slice().cmp(right.as_slice())
+            }
+        }
+    }
+}
+impl PartialOrd for ListOrInt {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+fn parse_list(input: &str) -> Vec<ListOrInt> {
+    fn _parse_list(mut input: &str) -> (Vec<ListOrInt>, &str) {
+        let mut list = Vec::new();
+        assert!(input.starts_with('['));
+        input = &input[1..];
+        while !input.starts_with(']') {
+            let (list_or_int, rem) = _parse_list_or_int(input);
+            list.push(list_or_int);
+            input = rem;
+
+            if input.starts_with(',') {
+                input = &input[1..];
+            }
+        }
+        assert!(input.starts_with(']'));
+        (list, &input[1..])
+    }
+
+    fn _parse_list_or_int(input: &str) -> (ListOrInt, &str) {
+        match &input[..1] {
+            "[" => {
+                let (list, input) = _parse_list(input);
+                (ListOrInt::List(list), input)
+            }
+            _ => {
+                let end = input
+                    .as_bytes()
+                    .iter()
+                    .copied()
+                    .position(|b| !b.is_ascii_digit())
+                    .unwrap();
+                let num: usize = input[..end].parse().unwrap();
+                (ListOrInt::Int(num), &input[end..])
+            }
+        }
+    }
+    let (l, rem) = _parse_list(input);
+    assert!(rem.is_empty());
+    l
+}
+
+#[aoc(day13, part1)]
+pub fn part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .enumerate()
+        .filter_map(|(idx, packet_pair)| {
+            let (first, second) = packet_pair.split_once('\n').unwrap();
+            let first = parse_list(first);
+            let second = parse_list(second);
+            first.cmp(&second).is_lt().then_some(idx + 1)
+        })
+        .sum()
+}
+
+#[aoc(day13, part2)]
+pub fn part2(input: &str) -> usize {
+    let mut packets: Vec<_> = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(parse_list)
+        .collect();
+    let divider1 = vec![ListOrInt::List(vec![ListOrInt::Int(2)])];
+    let divider2 = vec![ListOrInt::List(vec![ListOrInt::Int(6)])];
+    packets.push(divider1.clone());
+    packets.push(divider2.clone());
+    packets.sort();
+    (packets.binary_search(&divider1).unwrap() + 1)
+        * (packets.binary_search(&divider2).unwrap() + 1)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod day1;
 pub mod day10;
 pub mod day11;
 pub mod day12;
+pub mod day13;
 pub mod day2;
 pub mod day3;
 pub mod day4;
@@ -11,7 +12,6 @@ pub mod day7;
 pub mod day8;
 pub mod day9;
 /*
-pub mod day13;
 pub mod day14;
 pub mod day15;
 pub mod day16;
